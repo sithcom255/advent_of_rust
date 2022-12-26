111f1ee165f455bf7c19bbe971562e43e40c68a0
sithcom255 <honza.gavlik@gmail.com>
2022-12-26 20:48:07 +0100

    day_3_one

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day_one;
 mod day_two;
+mod day_3;
 
 fn main () {
-    day_two::day_two("/home/jan/Documents/advent_of_rust/src/input")
+    day_3::advent_of_rust("/home/jan/Documents/advent_of_rust/src/input")
 }
