6f45207e69556dc4a3fcb2da4aa418404f179a3b
sithcom255 <honza.gavlik@gmail.com>
2022-12-26 08:23:00 +0100

    day_two_one

---
+++ src/day_two.rs
@@ -0,0 +1,106 @@
+use std::env;
+use std::fs;
+use std::str::FromStr;
+
+pub fn day_two(file_path: &str) {
+    let contents = fs::read_to_string(file_path).expect("This is the error message");
+    let rows = contents.split("\n");
+    let mut final_res = 0;
+    rows.for_each(|row: &str| {
+        let vec = row.split(" ").collect::<Vec<&str>>();
+        let other = Other::from_str(vec[0]).unwrap();
+        let response = Response::from_str(vec[1]).unwrap();
+        final_res += evaluate(other, response);
+    });
+    print!("{}", final_res);
+}
+
+fn evaluate(other: Other, response: Response) -> u32 {
+    let mut res = 0;
+
+    if response == Response::X {
+        res += 1;
+    } else if response == Response::Y {
+        res += 2;
+    } else {
+        res += 3;
+    }
+
+    if other == Other::A {
+        if response == Response::X {
+            res += 3;
+        } else if response == Response::Y {
+            res += 6;
+        } else if response == Response::Z {
+            res += 0;
+        }
+    }
+    if other == Other::B {
+        if response == Response::X {
+            res += 0;
+        } else if response == Response::Y {
+            res += 3;
+        } else if response == Response::Z {
+            res += 6;
+        }
+    }
+
+    if other == Other::C {
+        if response == Response::X {
+            res += 6;
+        } else if response == Response::Y {
+            res += 0;
+        } else if response == Response::Z {
+            res += 3;
+        }
+    }
+
+    return res;
+}
+
+#[derive(PartialEq)]
+enum Response {
+    // rock
+    X,
+    // paper
+    Y,
+    // scissors
+    Z,
+}
+
+impl FromStr for Response {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Response, Self::Err> {
+        match s {
+            "X" => Ok(Response::X),
+            "Y" => Ok(Response::Y),
+            "Z" => Ok(Response::Z),
+            _ => Err(()),
+        }
+    }
+}
+#[derive(PartialEq)]
+enum Other {
+    // rock
+    A,
+    // paper
+    B,
+    // scissors
+    C,
+}
+
+impl FromStr for Other {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Other, Self::Err> {
+        match s {
+            "A" => Ok(Other::A),
+            "B" => Ok(Other::B),
+            "C" => Ok(Other::C),
+            _ => Err(()),
+        }
+    }
+}
+
+
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod day_one;
+mod day_two;
+
+fn main () {
+    day_two::day_two("/home/jan/Documents/advent_of_rust/src/input")
+}
